62e8b662a844be77a2cf632303f7e1fc40109277
guiakiraa <guilhermeakira0810@gmail.com>
2024-05-19 19:09:43 -0300

    menu responsivo

--- projeto/html/cadastro_automovel/js/script.js
+++ projeto/html/cadastro_automovel/js/script.js
@@ -1,3 +1,14 @@
+function mostrarMenu() {
+    let menuCelular = document.querySelector(".menu-celular")
+    if (menuCelular.classList.contains("open")) {
+        menuCelular.classList.remove("open")
+        document.querySelector("#icone").src = "../../img/menu_white_36dp.svg"
+    } else {
+        menuCelular.classList.add("open")
+        document.querySelector("#icone").src = "../../img/close_white_36dp.svg"
+    }
+}
+
 const form = document.querySelector("form")
 
 form.addEventListener("submit", function(e) {
